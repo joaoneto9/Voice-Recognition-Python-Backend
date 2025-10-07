241a16ffb3a46b31b9c49100c032bdfb91ea55f8
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2025-10-06 23:09:30 -0300

    modularizando melhor o sistema e concentrando operações de áudio em uma classe AudioOperations

--- python_backend/services/audio_controller.py
+++ python_backend/services/audio_controller.py
@@ -1,11 +1,10 @@
-from aifc import Error
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import io
-from audio_utils.audio_transcribe import audio_transcription
-from pydub import AudioSegment
-import tempfile
-import os
+from audio_utils.audio_operations import AudioOperations
+
+HOST = '0.0.0.0'
+PORT = 5000
+KEY_REQUEST_FIELD = "audio"
 
 app = Flask(__name__)
 CORS(app)
@@ -15,40 +14,29 @@ def get_req():
     return "opa"
 
 @app.route("/transcribe", methods=["POST"])
-def transact_and_transcribe_audio():
-    
-    if "audio" not in request.files:
-        return jsonify({"erro": "Nenhum arquivo de áudio foi enviado"}), 400
-    
-    audio = request.files["audio"]
+def get_audio_transcribed():
 
-    if audio.filename == "":
-        return jsonify({"erro": "Nome de arquivo inválido"}), 400
+    if KEY_REQUEST_FIELD not in request.files:
+        return jsonify({"Error": "No audio file detected!"}), 400
 
-    webm_audio_bytes = io.BytesIO(audio.read())
+    audio_file = request.files[KEY_REQUEST_FIELD]
+
+    if audio_file.name == "":
+        return jsonify({"Error": "Invalid name for audio file!"}), 400
 
     try:
 
-        audio_segment = AudioSegment.from_file(webm_audio_bytes, format="webm")
+        audio_op = AudioOperations()
 
-        wav_audio_bytes = io.BytesIO()
+        wav_audio_file_path = audio_op.convert_to_wav(audio_op.get_file_bytes(audio_file))
 
-        audio_segment.export(wav_audio_bytes, format="wav")
+        final_transcription = audio_op.transcribe_audio(wav_audio_file_path)
 
-        wav_audio_bytes.seek(0)
+        return jsonify({"text": final_transcription}), 200
 
-        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_wav_file:
-            temp_wav_file.write(wav_audio_bytes.read())
-            temp_wav_file_path = temp_wav_file.name
-
-        audio_transcription_text = audio_transcription(temp_wav_file_path)
-
-        os.remove(temp_wav_file_path)
-
-        return jsonify({"text": audio_transcription_text})
     except Exception as e:
-        print(f"LOG: ERRO DURANTE O PROCESSAMENTO: {e}")
-        return jsonify({"erro": f"Erro: {e}"}), 400
+        print(f"LOG: ERROR WHILE PROCESSING: {e}")
+        return jsonify({"Error": {e}}), 400
 
 def run_app():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=HOST, port=PORT)
